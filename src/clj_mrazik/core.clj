a56955f4dc3ea085d9608e5a409f0675a9648005
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-03-12 22:50:13 +0100

    Loading configuration

--- src/clj_mrazik/core.clj
+++ src/clj_mrazik/core.clj
@@ -1,7 +1,14 @@
 (ns clj-mrazik.core
   (:gen-class))
 
+(require '[clojure.pprint :as pprint])
+
+(require '[irclj.core :as irc])
+
+(require '[clj-mrazik.config :as config])
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+    "Entry point to this bot."
+    [& args]
+    (pprint/pprint (config/load-configuration "config.ini")))
+
