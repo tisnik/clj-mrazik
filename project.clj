e6ae08f87e19d6955af1ed7daf11fc118a8f5b61
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-03-12 21:59:31 +0100

    Added licence text to project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,22 @@
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (defproject clj-mrazik "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [irclj "0.5.0-alpha4"]]
   :main ^:skip-aot clj-mrazik.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
