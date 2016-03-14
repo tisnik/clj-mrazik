aa561b06a3f16d73ecfb9c47ee5f84b3218984f6
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-03-14 17:18:32 +0100

    1st version of the bot logic

--- src/clj_mrazik/core.clj
+++ src/clj_mrazik/core.clj
@@ -21,6 +21,50 @@
 (require '[clj-mrazik.schedule :as schedule])
 (require '[clj-mrazik.irc-bot  :as irc-bot])
 
+(def sleep-amount
+    10000)
+
+(defn between?
+    [schedule-item minutes]
+    (and (>= minutes (-> schedule-item :minutes :from))
+         (<= minutes (-> schedule-item :minutes :to))))
+
+(defn in-schedule?
+    [schedule minutes]
+    (if (some #(between? % minutes) schedule)
+        :should-be-open
+        :should-be-closed))
+
+(defn open-window
+    [config]
+    (let [message    (-> config :bot :message1)
+          recipients (-> config :server :recipients)
+          channel    (-> config :server channel)]
+          (println message)
+          (irc-bot/send-message recipients channel message)))
+
+(defn close-window
+    [config]
+    (let [message    (-> config :bot :message2)
+          recipients (-> config :server :recipients)
+          channel    (-> config :server channel)]
+          (println message)
+          (irc-bot/send-message recipients channel message)))
+
+(defn run-bot
+    [config schedule]
+    (loop [status :window-closed]
+        (Thread/sleep sleep-amount)
+        (let [minutes (schedule/minute-of-day)
+              actual  (in-schedule? schedule minutes)]
+            (println "minutes: " minutes status actual)
+            (condp = [status actual]
+                [:window-closed :should-be-closed] (recur :window-closed)
+                [:window-closed :should-be-open]   (do (open-window config)  (recur :window-open))
+                [:window-open   :should-be-closed] (do (close-window config) (recur :window-closed))
+                [:window-open   :should-be-open]   (recur :window-open))
+        )))
+
 (defn -main
     "Entry point to this bot."
     [& args]
@@ -29,5 +73,7 @@
          (config/print-configuration config)
          (pprint/pprint schedule)
          (irc-bot/start-irc-bot (:server config))
+         (irc-bot/send-message (-> config :server :recipients) (-> config :server :channel) "Hi!")
+         (run-bot config schedule)
     ))
 
