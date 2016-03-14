bcc513a8e40e91b2ea7291f4e8cb45d3a28c22f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-03-14 14:38:32 +0100

    Added licence and removed test stub from core_test.clj

--- test/clj_mrazik/core_test.clj
+++ test/clj_mrazik/core_test.clj
@@ -1,7 +1,16 @@
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (ns clj-mrazik.core-test
   (:require [clojure.test :refer :all]
             [clj-mrazik.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
