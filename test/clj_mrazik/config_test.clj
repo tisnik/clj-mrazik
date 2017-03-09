3c1e9f0824225d23c448a9e9a02ccac8cc0111df
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-09 16:38:32 +0100

    Added six tests into config_test module

--- test/clj_mrazik/config_test.clj
+++ test/clj_mrazik/config_test.clj
@@ -32,15 +32,54 @@
     (testing "if the clj-mrazik.config/parse-int definition exists."
         (is (callable? 'clj-mrazik.config/parse-int))))
 
+
 (deftest test-parse-float-existence
     "Check that the clj-mrazik.config/parse-float definition exists."
     (testing "if the clj-mrazik.config/parse-float definition exists."
         (is (callable? 'clj-mrazik.config/parse-float))))
 
+
+(deftest test-parse-boolean-existence
+    "Check that the clj-mrazik.config/parse-boolean definition exists."
+    (testing "if the clj-mrazik.config/parse-boolean definition exists."
+        (is (callable? 'clj-mrazik.config/parse-boolean))))
+
+
+(deftest test-parse-hh-mm-time-as-minutes-existence
+    "Check that the clj-mrazik.config/parse-hh-mm-time-as-minutes definition exists."
+    (testing "if the clj-mrazik.config/parse-hh-mm-time-as-minutes definition exists."
+        (is (callable? 'clj-mrazik.config/parse-hh-mm-time-as-minutes))))
+
+
+(deftest test-update-server-configuration-existence
+    "Check that the clj-mrazik.config/update-server-configuration definition exists."
+    (testing "if the clj-mrazik.config/update-server-configuration definition exists."
+        (is (callable? 'clj-mrazik.config/update-server-configuration))))
+
+
+(deftest test-update-bot-configuration-existence
+    "Check that the clj-mrazik.config/update-bot-configuration definition exists."
+    (testing "if the clj-mrazik.config/update-bot-configuration definition exists."
+        (is (callable? 'clj-mrazik.config/update-bot-configuration))))
+
+
+(deftest test-update-geolocation-configuration-existence
+    "Check that the clj-mrazik.config/update-geolocation-configuration definition exists."
+    (testing "if the clj-mrazik.config/update-geolocation-configuration definition exists."
+        (is (callable? 'clj-mrazik.config/update-geolocation-configuration))))
+
+
+(deftest test-update-modules-configuration-existence
+    "Check that the clj-mrazik.config/update-modules-configuration definition exists."
+    (testing "if the clj-mrazik.config/update-modules-configuration definition exists."
+        (is (callable? 'clj-mrazik.config/update-modules-configuration))))
+
+
 (deftest test-load-configuration-existence
     "Check that the clj-mrazik.config/load-configuration definition exists."
     (testing "if the clj-mrazik.config/load-configuration definition exists."
         (is (callable? 'clj-mrazik.config/load-configuration))))
+
 
 (deftest test-print-configuration-existence
     "Check that the clj-mrazik.config/print-configuration definition exists."
