2a419aae2882e471ce3153535af493904e6df6fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-06-07 14:44:52 +0200

    Use log module, not printing to the standard output

--- src/clj_mrazik/irc_bot.clj
+++ src/clj_mrazik/irc_bot.clj
@@ -15,6 +15,8 @@
 (require '[irclj.core :as irc])
 (require '[clj-calendar.calendar :as calendar])
 
+(require '[clojure.tools.logging :as log])
+
 (require '[clj-mrazik.dyncfg   :as dyncfg])
 (require '[clj-mrazik.schedule :as schedule])
 (require '[clj-mrazik.dictionary :as dictionary])
@@ -202,7 +204,7 @@
         {:prefix prefix
          :response response})
         (catch Exception e
-            (println (.getMessage e))
+            (log/error e "prepare-reply-text")
             {:prefix ""
              :response ""})))
 
@@ -213,8 +215,8 @@
            nick    :nick
            host    :host
            command :command} incoming-message]
-           (println "Received message from" nick "to" target ":" text "(" host command ")")
-           (println incoming-message)
+           (log/info (str "Received message from" nick "to" target ":" text "(" host command ")"))
+           (log/info incoming-message)
            (if (message-for-me? @dyncfg/bot-nick (-> @dyncfg/configuration :bot :prefix) incoming-message)
                (let [reply  (create-reply incoming-message)
                      output (prepare-reply-text incoming-message nick text)]
@@ -238,14 +240,14 @@
           channels (:channels configuration)
           chanlist (clojure.string/split channels #" ")
           nick     (:nick configuration)]
-        (println "Connecting to" server "on port" port)
+        (log/info "Connecting to" server "on port" port)
         (let [conn (irc/connect server port nick
                                 :callbacks {:privmsg on-incoming-message})]
-            (println "Connected, joining to channels" channels)
+            (log/info "Connected, joining to channels" channels)
             (reset! dyncfg/connection conn)
             (reset! dyncfg/bot-nick nick)
             (doseq [channel chanlist]
-                (println channel)
+                (log/info channel)
                 (irc/join @dyncfg/connection (clojure.string/trim channel)))
-            (println "Connected..."))))
+            (log/info "Connected..."))))
 
